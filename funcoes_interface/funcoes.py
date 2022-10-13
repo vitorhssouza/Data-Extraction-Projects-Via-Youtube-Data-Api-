4f1ca1cc10d921982ccb437ff1e5f1cb279c43cf
Vitor Souza <vitorugo_kta@hotmail.com>
2022-10-13 15:42:14 -0300

    Criando arquivo funções e criando funções para o menu principal e o secundario

---
+++ funcoes_interface/funcoes.py
@@ -0,0 +1,34 @@
+# Primeiro menu de interface do programa
+
+from time import sleep
+
+def linha(tamanho=90):
+    print('-'*tamanho)
+
+
+def cabecalho():
+    linha()
+    print('Extraindo Dados Da Api Do Youtube')
+    linha()
+
+
+def menu_inicial():
+    cabecalho()
+    print('1 - Inserir o id do video ou a playlist que desaja analisar os dados \n'
+          '2 - Sair do sistema')
+    opcao: int = int(input('Informe sua opção: '))
+    menu_secundario(opcao)
+    pass
+
+
+def menu_secundario(opcao):
+    cabecalho()
+    if opcao == 1:
+        pass
+    elif opcao == 2:
+        exit()
+    else:
+        print('Opção inválida')
+        sleep(3)
+        menu_inicial()
+
