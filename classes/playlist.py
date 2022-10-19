4bd91bc0645473d3a66c04d494968b1b8707e3e4
Vitor Souza <vitorugo_kta@hotmail.com>
2022-10-19 18:35:04 -0300

    Criando metodo que retorna a lista com alguns dados da playlist fornecida pelo usuario

--- classes/playlist.py
+++ classes/playlist.py
@@ -1,3 +1,26 @@
+from classes.conexao import Conexao
+from chave_api import api_key
+
+youtube = Conexao(api_key)
+nextPage_token = None
+
+
 class Playlist:
 
-    pass
+    def __init__(self, url_playlist):
+        self.__url_playlist = url_playlist
+        self.__playlist = list()
+
+    @property
+    def playlist(self):
+        while True:
+            filtro = youtube.conexao.playlistItems().list(part='snippet', playlistId=api_key, maxResults=50,
+                                                          pageToken=nextPage_token).execute()
+            self.__playlist += filtro['items']
+            nextPage_token = filtro.get('nestPageToken')
+
+            if nextPage_token is None:
+                break
+        return self.__playlist
+
+
