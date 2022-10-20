c97a70a7fc0563c89c80f09289d31be98a126b35
Vitor Souza <vitorugo_kta@hotmail.com>
2022-10-20 18:32:41 -0300

    Criando a classe estatistica playlist

--- classes/estatistica.py
+++ classes/estatistica.py
@@ -3,11 +3,9 @@ from classes.playlist import Playlist
 from classes.videos import Videos
 
 
-class Estatistica(Playlist, Videos):
-    def __init__(self, url_playlist, url_videos):
+class EstatisticaPlaylist(Playlist):
+    def __init__(self, url_playlist):
         super().__init__(url_playlist)
-        super().__init__(url_videos)
-
         self.__titulos_videos = list()
         self.__data_publicacoes = list()
         self.__descricao_videos = list()
