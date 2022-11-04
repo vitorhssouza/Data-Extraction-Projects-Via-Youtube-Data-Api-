1513077666c2b9dd225732017e12ba1c2f5d7e04
Vitor Souza <vitorugo_kta@hotmail.com>
2022-11-04 19:21:59 -0300

    criando uma variavel dataframe

--- classes/dataframe.py
+++ classes/dataframe.py
@@ -1,20 +1,28 @@
 import pandas as pd
 from classes.estatistica import EstatisticaPlaylist
 
-dados = EstatisticaPlaylist()
 
+class EstruturaDataFrame(EstatisticaPlaylist):
 
-class EstruturaDataFrame:
+    def __init__(self, url_playlist):
+        super().__init__(url_playlist)
 
-    def __init__(self, titulo, id, descricao, data_publicacao, data_extracao, likes, visualizacao, comentario):
-        self.__tituto = titulo
-        self.__id = id
-        self._descricao = descricao
-        self.__data_publicacao = data_publicacao
-        self._data_extracao = data_extracao
-        self.__likes = likes
-        self.__visualizacao = visualizacao
-        self.__comentario = comentario
+        self.__dataframe = pd.DataFrame({
+                            'Título': self.titulos_videos,
+                            'Id Video': self.id_videos,
+                            'Descricao do Video': self.descricao_videos,
+                            'Data de Publicacao': self.data_publicacao,
+                            'Data de Extração': self.data_publicacao,
+                            'Likes': self.likes,
+                            'Visualizações': self.visualizacao,
+                            'Comentarios': self.comentarios,
+                            'Url dos Videos': self.url_videos
+                            })
+
+    @property
+    def dataframe(self):
+        return self.__dataframe['Título']
+
 
 
 
