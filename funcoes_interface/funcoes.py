74ce4bbee632708e5cd144463e2eaec6230c7067
Vitor Souza <vitorugo_kta@hotmail.com>
2022-11-09 19:20:28 -0300

    Fazendo melhorias no arquivo função

--- funcoes_interface/funcoes.py
+++ funcoes_interface/funcoes.py
@@ -2,8 +2,11 @@
 
 from classes.videos import Videos
 from classes.playlist import Playlist
+from classes.estatistica import EstatisticaPlaylist
+from classes.dataframe import EstruturaDataFrame
 
 from time import sleep
+
 
 def linha(tamanho=90):
     print('='*tamanho)
@@ -27,12 +30,21 @@ def menu_inicial():
 def menu_secundario(opcao):
     cabecalho()
     if opcao == 1:
-        url: str = str(input('Inserir o id do video ou a playlist que desaja analisar os dados: '))
+        url: str = str(input('Inserir o id do video ou a playlist que deseja analisar os dados: '))
         if len(url) > 15:
-            playlist = Playlist(url)
-            print(playlist.playlist)
+            print('1 - Dados\n'
+                  '2 - Total de visualições por video\n'
+                  '3 - Total de likes por videos\n'
+                  '4 - Total de comentario por video\n'
+                  '5 - Top 10 videos com mais visualizações\n'
+                  '6 - Relação entre visualizações e likes\n'
+                  '7 - Visualização preditiva\n'
+                  '8 - Sair do programa')
+            op: int = int(input("Informe sua opção: "))
+            menu_opcao(url=url, opcao=op)
         else:
-            videos = Videos(url)
+            """videos = Videos(url)"""
+            pass
     elif opcao == 2:
         exit()
     else:
@@ -40,3 +52,27 @@ def menu_secundario(opcao):
         sleep(1)
         menu_inicial()
 
+
+def menu_opcao(url, opcao):
+    if opcao == 1:
+        pass
+    elif opcao == 2:
+        pass
+    elif opcao == 3:
+        pass
+    elif opcao == 4:
+        pass
+    elif opcao == 5:
+        pass
+    elif opcao == 6:
+        pass
+    elif opcao == 7:
+        pass
+    elif opcao == 8:
+        print('Saindo do sistema')
+        sleep(3)
+        exit()
+    else:
+        print('Opção inválida')
+        sleep(2)
+        """menu_opcao()"""
